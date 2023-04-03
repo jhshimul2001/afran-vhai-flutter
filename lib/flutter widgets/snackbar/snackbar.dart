29a22f42799757a317be69110d20843728a6ddf9
juvaid hasan shimul <119728892+jhshimul2001@users.noreply.github.com>
2023-04-04 02:18:18 +0600

    Update snackbar.dart

--- lib/flutter widgets/snackbar/snackbar.dart
+++ lib/flutter widgets/snackbar/snackbar.dart
@@ -1,35 +1,35 @@
-// import 'package:flutter/material.dart';
-//
-// class Snackbar extends StatefulWidget {
-//   const Snackbar({Key? key}) : super(key: key);
-//
-//   @override
-//   State<Snackbar> createState() => _SnackbarState();
-// }
-//
-// class _SnackbarState extends State<Snackbar> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(child: Scaffold(
-//       appBar: AppBar(
-//         title: Text("Afran sarkar flutter tutorial"),
-//         leading: Icon(Icons.arrow_back),
-//       ),
-//       body: Center(
-//         child: Builder(
-//           builder: (BuildContext context) {
-//             return MaterialButton(
-//               onPressed: (){
-//                 Scaffold.of(context).showSnackBar(SnackBar(content: Text('snackbar'),
-//                   duration: Duration(seconds: 10),
-//                   action: SnackBarAction(label: 'ok',onPressed: (){}),
-//                 ));
-//               },
-//               child: Text("click"),
-//             );
-//           }
-//         ),
-//       ),
-//     ));
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class Snackbar extends StatefulWidget {
+  const Snackbar({Key? key}) : super(key: key);
+
+  @override
+  State<Snackbar> createState() => _SnackbarState();
+}
+
+class _SnackbarState extends State<Snackbar> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      appBar: AppBar(
+        title: Text("Afran sarkar flutter tutorial"),
+        leading: Icon(Icons.arrow_back),
+      ),
+      body: Center(
+        child: Builder(
+          builder: (BuildContext context) {
+            return MaterialButton(
+              onPressed: (){
+                Scaffold.of(context).showSnackBar(SnackBar(content: Text('snackbar'),
+                  duration: Duration(seconds: 10),
+                  action: SnackBarAction(label: 'ok',onPressed: (){}),
+                ));
+              },
+              child: Text("click"),
+            );
+          }
+        ),
+      ),
+    ));
+  }
+}
